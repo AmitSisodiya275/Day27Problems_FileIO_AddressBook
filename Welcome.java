907d9105368b6b523710dcebf53566df02de8a1a
AmitSisodiya275 <amitsisodiya275@gmail.com>
2021-11-18 14:56:55 +0530

    Added: a welcome file which can print the welcome message on the console.

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+package com.bridgelab.addressbook.fileio;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to day 27 Problems of File IO in Address Book.");
+	}
+
+}
